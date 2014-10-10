281cae0bcb39f11df8cc9ddbf733a9fa66019cde
s <sonnykjellberg@gmail.com>
2014-10-10 16:54:14 +0200

    random commit

--- digitalvackarklockaB/digitalvackarklockaB/Program.cs
+++ digitalvackarklockaB/digitalvackarklockaB/Program.cs
@@ -10,24 +10,46 @@ namespace digitalvackarklockaB
     {
         static void Main(string[] args)
         {
+            //Instansiera objekt av klassen AlarmClock - 
+            //testa konstruktorerna, egenskaperna och metoderna.
+            
 
-            //testfest
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔══════════════════════════════════════╗ ");
+            Console.WriteLine(" ║           Väckarklockan              ║ ");
+            Console.WriteLine(" ║        BEEP BEEP BEEP BEEP           ║ ");
+            Console.WriteLine(" ╚══════════════════════════════════════╝ ");
+            Console.ResetColor();
+            Console.WriteLine();
+
+
+
+
+            //testfest:
+
+            //Test 2:
+            AlarmClock ac = new AlarmClock(9, 42);
+            ViewTestHeader("\n════════════════════════════════════════\n\nTest 2.\nTest av konstruktorn med två parametrar, <9, 42>.");
+            Run(ac, 4);
+            Console.WriteLine("\n{0}\n", ac);
+
         }
         private static void Run(AlarmClock ac, int minutes)
         {
             for (int i = 0; i < minutes; i++)
-            {
-                //if (ac.TickTock())
-                //{
-                //    Console.BackgroundColor = ConsoleColor.Blue;
-                //    Console.ForegroundColor = ConsoleColor.White;
-                //    Console.WriteLine("{0} BEEP BEEP", ac;
-                //    Console.ResetColor();
-                //}
-                //else
-                //{
-                //    Console.WriteLine(ac);
-                //}
+            {                
+                if (ac.TickTock())
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("{0} BEEP BEEP", ac);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(ac);
+                }
                 Console.WriteLine(ac);
             }
         }
@@ -35,6 +57,8 @@ namespace digitalvackarklockaB
 
         private static void ViewTestHeader(string header)      //Används för att skriva ut meddelanden under testkörningen.
         {
+            string horizontalLine = "════════════════════════════════════════";
+            Console.WriteLine(horizontalLine);
             Console.WriteLine(header);
         }
 
