69639887f87e5dca927be323b6d45ab0b81e376e
s <sonnykjellberg@gmail.com>
2014-10-10 12:31:16 +0200

    Fields and get-sets in AlarmClock.cs

--- digitalvackarklockaB/digitalvackarklockaB/AlarmClock.cs
+++ digitalvackarklockaB/digitalvackarklockaB/AlarmClock.cs
@@ -8,5 +8,33 @@ namespace digitalvackarklockaB
 {
     class AlarmClock
     {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set { _alarmHour = value; }
+        }
+
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set { _alarmMinute = value; }
+        }
+
+        public int Hour
+        {
+            get { return _hour; }
+            set { _Hour = value; }
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set { _minute = value; }
+        }
     }
 }
